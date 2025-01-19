2e431b757ddc5e2ea51768d7802e773ff2f753b2
Panjulhaq <haqonijul24@gmail.com>
2025-01-19 19:33:53 +0700

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,212 @@
+import KelasImage1 from "../assets/img/kelas/kelas-1.jpg";
+import KelasImage2 from "../assets/img/kelas/kelas-2.jpg";
+import KelasImage3 from "../assets/img/kelas/kelas-3.jpg";
+import KelasImage4 from "../assets/img/kelas/kelas-4.jpg";
+import KelasImage5 from "../assets/img/kelas/kelas-5.jpg";
+import KelasImage6 from "../assets/img/kelas/kelas-6.jpg";
+import KelasImage7 from "../assets/img/kelas/kelas-7.jpg";
+import KelasImage8 from "../assets/img/kelas/kelas-8.jpg";
+
+import People1 from "../assets/img/testimonial/people-1.jpg";
+import People2 from "../assets/img/testimonial/people-2.jpg";
+import People3 from "../assets/img/testimonial/people-3.jpg";
+
+export const navLinks = [
+  {
+    id: 1,
+    path: "",
+    text: "Home",
+  },
+  {
+    id: 2,
+    path: "paket",
+    text: "Paket",
+  },
+  {
+    id: 3,
+    path: "faq",
+    text: "FAQ",
+  },
+  {
+    id: 4,
+    path: "syaratketen",
+    text: "Syarat & Ketentuan",
+  },
+];
+
+export const semuaKelas = [
+  {
+    id: 1,
+    image: KelasImage1,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "Jasa Pengurusan Kedukaan On Demand VIP Standard",
+    price: "Rp. 25.000.000",
+    buy: "Pesan Layanan",
+    delay: "1300",
+  },
+  {
+    id: 2,
+    image: KelasImage2,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "Jasa Pengurusan Kedukaan On Demand VIP",
+    price: "Rp. 85.000.000",
+    buy: "Pesan Layanan",
+    delay: "1600",
+  },
+  {
+    id: 3,
+    image: KelasImage3,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "Jasa Pengurusan Kedukaan On Demand Premium",
+    price: "Rp. 50.000.000",
+    buy: "Pesan Layanan",
+    delay: "1900",
+  },
+  {
+    id: 4,
+    image: KelasImage4,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "Sewa Ambulance Jenazah",
+    price: "Rp. 500.000",
+    buy: "Pesan Layanan",
+    delay: "2100",
+  },
+  {
+    id: 5,
+    image: KelasImage5,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "proteksi kedukaan berjangka Aman",
+    price: "Rp. 25.000.000",
+    buy: "Pesan Layanan",
+    delay: "1000",
+  },
+  {
+    id: 6,
+    image: KelasImage6,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "proteksi kedukaan berjangka Nyaman",
+    price: "Rp. 50.000.000",
+    buy: "Pesan Layanan",
+    delay: "1300",
+  },
+  {
+    id: 7,
+    image: KelasImage7,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "proteksi kedukaan berjangka Tentram",
+    price: "Rp. 100.000.000",
+    buy: "Pesan Layanan",
+    delay: "1600",
+  },
+  {
+    id: 8,
+    image: KelasImage8,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "Jasa Pengurusan Akta Kematian",
+    price: "Rp. 500.000",
+    buy: "Pesan Layanan",
+    delay: "1900",
+  },
+];
+
+export const kelasTerbaru = [
+  {
+    id: 1,
+    image: KelasImage1,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "Jasa Pengurusan Kedukaan On Demand Standard",
+    price: "Rp. 25.000.000",
+    buy: "Pesan Layanan",
+    delay: "",
+  },
+  {
+    id: 2,
+    image: KelasImage2,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "Jasa Pengurusan Kedukaan On Demand VIP",
+    price: "Rp. 50.000.000",
+    buy: "Pesan Layanan",
+    delay: "200",
+  },
+  {
+    id: 3,
+    image: KelasImage3,
+    star1: "fa-solid fa-star",
+    star2: "fa-solid fa-star",
+    star3: "fa-solid fa-star",
+    star4: "fa-solid fa-star",
+    star5: "fa-solid fa-star",
+    title: "Jasa Pengurusan Kedukaan On Demand Premium",
+    price: "Rp. 85.000.000",
+    buy: "Pesan Layanan",
+    delay: "400",
+  },
+];
+
+export const faq = [
+  {
+    id: 1,
+    eventKey: 0,
+    title:
+      "Perlukah keluarga / ahli waris menyelesaikan biaya atas keperluan kedukaan saat anggota meninggal dunia saat dana proteksi yang terbayar belum mencukupi tipe proteksi yang dipilih?",
+    desc: "Manfaat dan layanan kedukaan bagi anggota akan sepenuhnya diberikan oleh kami dan diterima oleh keluarga ketika anggota meninggal dunia, sesuai dengan tipe proteksi yang telah dipilih saat pendaftaran, meskipun jumlah dana yang dibayarkan anggota belum mencapai nilai anggaran proteksi tersebut. Selain itu, keluarga akan dibebaskan dari seluruh biaya, baik untuk layanan kedukaan maupun untuk periode proteksi kedukaan anggota berikutnya.",
+  },
+  {
+    id: 2,
+    eventKey: 1,
+    title: "Apakah kematian karena kecelakaan dan Covid-19 akan ditanggung?",
+    desc: "Ya. Kematian dikarenakan kecelakaan akan sepenuhnya ditanggung oleh Kami, bahkan tanpa harus melalui Masa Tunggu proteksi, sehingga dapat diklaim kapanpun jika kedukaan terjadi.Kematian dikarenakan Covid-19 atau virus Corona akan sepenuhnya ditanggung oleh Kami. Tertanggung setidaknya telah menjadi anggota Kamboja selama 45 hari.",
+  },
+  {
+    id: 3,
+    eventKey: 2,
+    title: "Berapa biaya Layanan Kedukaan On Demand?",
+    desc: "Biaya Layanan Kedukaan Lengkap tergantung dari paket yang dipilih. Kami memiliki paket layanan kedukaan mulai dari Standar, Premium hingga VIP.Harga disesuaikan juga dengan situasi jam kerja dan area tertentu. Silahkan hubungi Customer Support kami untuk meminta penawaran biaya bagi momen kedukaan Anda.",
+  },
+  {
+    id: 4,
+    eventKey: 3,
+    title: "Bagaimana cara kerja Layanan Kedukaan On Demand?",
+    desc: "Saat kedukaan tiba (atau diperkirakan akan tiba dalam waktu dekat), Anda dapat menghubungi customer support atau hotline untuk berkonsultasi mengenai perencanaan pemakaman atau langsung memilih paket yang telah disediakan. Customer support akan memberikan Anda formulir penawaran untuk diisi. Pastikan Anda mengisi semua informasi dan layanan secara lengkap. Selanjutnya, Anda akan menerima penawaran beserta total biaya yang diperlukan melalui email. Jika Anda setuju dengan penawaran tersebut, Anda dapat melakukan pembayaran uang muka agar layanan dapat segera dikoordinasi dan dilaksanakan. Funeral Manager akan segera menghubungi Anda setelah penawaran disetujui untuk memberikan detail proses pelaksanaan.",
+  },
+];
